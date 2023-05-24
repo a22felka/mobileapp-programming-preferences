1be60f111b0375a32d2b39e1d044740e986962af
a22felka <a22felka@student.his.se>
2023-05-24 11:59:15 +0200

    Gjorde så att knappen i SecondActivity tar användaren till MainActivity och kan skicka data till SharedPreferances

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,12 +3,31 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button = findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Save data to Shared Preferences
+                SharedPreferences sharedPreferences = getSharedPreferences("shared_preferances", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("struffies", "future_data_value");
+                editor.apply();
+
+                finish();
+            }
+        });
     }
 }
